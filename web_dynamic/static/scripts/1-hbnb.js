91917caf0d755ba0ecf87b7264ff9323166db18c
AymanAegon <ayo12ammar24@gmail.comnode>
2023-11-03 19:08:40 +0100

    fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
   let amenities_checked = [];
-  var checkbox = $('.amenity-checkbox');
+  var checkbox = $('input[type=checkbox]');
+
   checkbox.change(function () {
     $('.amenities h4').text('');
-    let amenity_id = checkbox.attr('data-id');
-    if (checkbox.is(':checked')) {
+    let amenity_id = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
       amenities_checked.push(amenity_id);
     } else {
       var index = amenities_checked.indexOf(amenity_id);
@@ -12,9 +13,9 @@ $(document).ready(function () {
     }
     for (let i = 0; i < amenities_checked.length; i++) {
       if (i === 0) {
-        $('.amenities h4').append(checkbox.attr('data-name'));
+        $('.amenities h4').append($(this).attr('data-name'));
       } else {
-        $('.amenities h4').append(', ' + checkbox.attr('data-name'));
+        $('.amenities h4').append(', ' + $(this).attr('data-name'));
       }
     }
   });
