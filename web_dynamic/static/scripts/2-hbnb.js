d664635eaf6cc869e05e4da83d5d874def30d87d
AymanAegon <ayo12ammar24@gmail.comnode>
2023-11-03 20:48:32 +0100

    fixing 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,10 +16,10 @@ $(document).ready(function () {
     }
     $('.amenities h4').text(amenities_names.join(', '));
   });
+// task 3
   const url = 'http://0.0.0.0:5001/api/v1/status/'
   $.get(url, function (data, textStatus, jqXHR) {
-    console.log(textStatus);
-    if (textStatus === "OK") {
+    if (data.status === "OK") {
       $("div#api_status").addClass("available");
     } else {
       $("div#api_status").addClass("available");
