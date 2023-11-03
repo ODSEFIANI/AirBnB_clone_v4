3b75e83e71d102585f1615142f4c37440f70ef9d
AymanAegon <ayo12ammar24@gmail.comnode>
2023-11-03 18:51:50 +0100

    fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   let amenities_checked = [];
   var checkbox = $('.amenity-checkbox');
   checkbox.change(function () {
+    $('.amenities h4').text('');
     let amenity_id = checkbox.attr('data-id');
     if (checkbox.is(':checked')) {
       amenities_checked.push(amenity_id);
@@ -9,8 +10,12 @@ $(document).ready(function () {
       var index = amenities_checked.indexOf(amenity_id);
       amenities_checked.splice(index, 1);
     }
-    amenities_checked.forEach(id => {
-      $('.amenities h4').text(id);
-    });
+    for (let i = 0; i < amenities_checked.length; i++) {
+      if (i === 0) {
+        $('.amenities h4').append(checkbox.attr('data-name'));
+      } else {
+        $('.amenities h4').append(', ' + checkbox.attr('data-name'));
+      }
+    }
   });
 });
