4942c1fcffd55a7d27bc637ef5bc36a0ad544a80
AymanAegon <ayo12ammar24@gmail.comnode>
2023-11-03 19:12:45 +0100

    fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,19 @@
 $(document).ready(function () {
   let amenities_checked = [];
   var checkbox = $('input[type=checkbox]');
-
+  let amenities_names = [];
   checkbox.change(function () {
     $('.amenities h4').text('');
     let amenity_id = $(this).attr('data-id');
+    let name = $(this).attr('data-name');
     if ($(this).is(':checked')) {
-      amenities_checked.push(amenity_id);
+        amenities_checked.push(amenity_id);
+        amenities_names.push(name);
     } else {
-      var index = amenities_checked.indexOf(amenity_id);
-      amenities_checked.splice(index, 1);
+        var index = amenities_checked.indexOf(amenity_id);
+        amenities_checked.splice(index, 1);
+        amenities_names.splice(index, 1);
     }
-    for (let i = 0; i < amenities_checked.length; i++) {
-      if (i === 0) {
-        $('.amenities h4').append($(this).attr('data-name'));
-      } else {
-        $('.amenities h4').append(', ' + $(this).attr('data-name'));
-      }
-    }
+    $('.amenities h4').text(amenities_names.join(', '));
   });
 });
